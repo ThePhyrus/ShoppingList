730ce696569b683678dc93f99009469321f31079
ThePhyrus <romaisgood@yandex.ru>
2022-03-26 00:55:00 +0300

    6. Добавил BottomNavigationMenu. Удалил themes.xml(night). Изменил некоторые цвета. Добавил иконки для нижнего меню. Добавил функцию setBottomNavListener() (заглушки логами).

--- app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
@@ -2,11 +2,41 @@ package com.example.shoppinglist.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.ActivityMainBinding
+
+
+private const val TAG: String = "@@@"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setBottomNavListener()
+    }
+
+    private fun setBottomNavListener() {
+        binding.bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Log.d(TAG, "settings")
+                }
+                R.id.notes -> {
+                    Log.d(TAG, "notes")
+                }
+                R.id.shop_list -> {
+                    Log.d(TAG, "shop_list")
+                }
+                R.id.new_item -> {
+                    Log.d(TAG, "new_item")
+                }
+            }
+            true
+        }
     }
 }
