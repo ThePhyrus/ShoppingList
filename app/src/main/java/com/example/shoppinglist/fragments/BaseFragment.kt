ef36886b477b4d763993da34c1de5f57787432dc
ThePhyrus <romaisgood@yandex.ru>
2022-03-26 16:44:10 +0300

    7. Добавил пару вспомогательных классов BaseFragment.kt и FragmentManager.kt. Добавил в разметку placeHolder (там попеременно будут сидеть фрагменты).

---
+++ app/src/main/java/com/example/shoppinglist/fragments/BaseFragment.kt
@@ -0,0 +1,10 @@
+package com.example.shoppinglist.fragments
+
+import androidx.fragment.app.Fragment
+
+// вспомогательный класс 1, который поможет управлять фрагментами
+// классы-наследники будут иметь fun onClickNew(), но она будет выполнять разные действия
+// полиморфизм в действии!!! ))
+abstract class BaseFragment : Fragment() {
+    abstract fun onClickNew()
+}
---
+++ app/src/main/java/com/example/shoppinglist/fragments/FragmentManager.kt
@@ -0,0 +1,38 @@
+package com.example.shoppinglist.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoppinglist.R
+
+// вспомогательный класс 2, который поможет управлять фрагментами
+// служит для переключения между фрагментами
+
+object FragmentManager {
+
+    /*
+    "object" для того, чтобы можно было использовать fun setFragment() без создания экземпляра
+    класса  FragmentManager.kt
+     */
+
+    var currentFrag: BaseFragment? = null
+    /*
+    Назначение currentFrag:
+    Когда в активити будет помещён фрагмент, необходимо будет знать, какой именно фрагмент работает
+    в ней на данный момент. Если нужно будет поместить другой фрагмент, то один (рабочий) убирается,
+    а на его место помещается другой.
+     */
+
+    fun setFragment(newFrag: BaseFragment, activity: AppCompatActivity) {
+
+        /*
+        При помощи этой функции можно будет переключаться между фрагментами. Аргументами функции
+        будут новый фрагмент, который нужно поместить в активити и контекст этой активити. Все
+        фрагменты, котрые можно будет сюда передавать, должны быть наследниками нашего
+        BaseFragment.kt (это у него одна наша функция, которая будет выполнять разные действия).
+         */
+
+        val transaction = activity.supportFragmentManager.beginTransaction() //переключает фрагменты
+        transaction.replace(R.id.placeHolder, newFrag) // указатель места, куда поместить фрагмент
+        transaction.commit() // подтвердить транзакцию (без этого счастья не будет)
+        currentFrag = newFrag // помещаем новый фрагмент в currentFrag
+    }
+}
