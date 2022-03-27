65c42bebe53874852779f9a1f6c52ffb6c82cd11
ThePhyrus <romaisgood@yandex.ru>
2022-03-27 20:05:56 +0300

    12. Создал ActivityResultLauncher for NoteFragment

--- app/src/main/java/com/example/shoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/NewNoteActivity.kt
@@ -1,11 +1,13 @@
 package com.example.shoppinglist.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.example.shoppinglist.R
 import com.example.shoppinglist.databinding.ActivityNewNoteBinding
+import com.example.shoppinglist.fragments.NoteFragment
 
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var binding:ActivityNewNoteBinding
@@ -22,12 +24,21 @@ class NewNoteActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home){
-            finish()
-        } else if (item.itemId == R.id.id_save){
+        if (item.itemId == R.id.id_save){
+           setMainResult()
+        } else if ( item.itemId == android.R.id.home){
             finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMainResult(){
+        val i = Intent().apply {
+            putExtra(NoteFragment.TITLE_KEY, binding.edTitle.text.toString())
+            putExtra(NoteFragment.DESC_KEY, binding.edDescription.text.toString())
+        }
+        setResult(RESULT_OK, i)
+        finish()
     }
 
     private fun actionBarSettings(){
--- app/src/main/java/com/example/shoppinglist/fragments/NoteFragment.kt
+++ app/src/main/java/com/example/shoppinglist/fragments/NoteFragment.kt
@@ -1,11 +1,16 @@
 package com.example.shoppinglist.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import com.example.shoppinglist.R
 import com.example.shoppinglist.activities.MainApp
@@ -14,21 +19,27 @@ import com.example.shoppinglist.databinding.FragmentNoteBinding
 import com.example.shoppinglist.db.MainViewModel
 
 //todo добавить описание работы класса
+
+private const val TAG: String = "@@@"
+
 class NoteFragment : BaseFragment() {
 
     private var _binding: FragmentNoteBinding? = null //FIXME не будет ли утечки?
     private val binding: FragmentNoteBinding get() = _binding!!
+
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>
 
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
 
     override fun onClickNew() {
-        startActivity(Intent(activity, NewNoteActivity::class.java))
+        editLauncher.launch(Intent(activity, NewNoteActivity::class.java))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onEditResult()
     }
 
     override fun onCreateView(
@@ -39,7 +50,21 @@ class NoteFragment : BaseFragment() {
         return binding.root
     }
 
+    private fun onEditResult() {
+        editLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                Log.d(TAG, "title: ${it.data?.getStringExtra(TITLE_KEY)} ")
+                Log.d(TAG, "description: ${it.data?.getStringExtra(DESC_KEY)} ")
+            }
+        }
+    }
+
     companion object { // нужен, чтобы сделать синглтон (будет только одна INSTANCE фрагмента)
+        const val TITLE_KEY = "title_key"
+        const val DESC_KEY = "desc_key"
+
         @JvmStatic
         fun newInstance() = NoteFragment()
     }
