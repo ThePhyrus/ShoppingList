ce57a3100c10e21697d628358066d4b1c1bd2815
ThePhyrus <romaisgood@yandex.ru>
2022-07-14 18:08:18 +0300

    32 - Закончил создание ShopListActivity.kt

--- app/src/main/java/com/example/shoppinglist/activities/ShopListActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/ShopListActivity.kt
@@ -3,6 +3,7 @@ package com.example.shoppinglist.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import com.example.shoppinglist.R
 import com.example.shoppinglist.databinding.ActivityShopListBinding
@@ -12,6 +13,7 @@ import com.example.shoppinglist.entities.ShopListNameItem
 class ShopListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityShopListBinding
     private var shopListNameItem: ShopListNameItem? = null
+    private lateinit var saveItem: MenuItem
 
     private val mainViewModel: MainViewModel by viewModels {
         MainViewModel.MainViewModelFactory((applicationContext as MainApp).database)
@@ -26,7 +28,27 @@ class ShopListActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.shop_list_menu, menu)
+        saveItem = menu?.findItem(R.id.save_item)!!
+        val newItem = menu.findItem(R.id.new_item)
+        newItem.setOnActionExpandListener(expandActionView())
+        saveItem.isVisible = false
         return true
+    }
+
+    private fun expandActionView () : MenuItem.OnActionExpandListener{
+        return object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                saveItem.isVisible = true
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                saveItem.isVisible = false
+                invalidateOptionsMenu()
+                return true
+            }
+
+        }
     }
 
     private fun init () {
